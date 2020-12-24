2b59c351d6ccb3b56db9a5b2148562cccef85ba7
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2020-12-24 20:54:37 +0000

    Cleaned up some codes, added tweets to gitignore

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,36 +67,16 @@
             </div>
     </header>
 
-    <!-- Topic Nav -->
-    <!-- <nav class="w-full py-4 border-t border-b bg-gray-100" x-data="{ open: false }">
-        <div class="block sm:hidden">
-            <a href="#" class="block md:hidden text-base font-bold uppercase text-center flex justify-center items-center" @click="open = !open">
-                Topics <i :class="open ? 'fa-chevron-down': 'fa-chevron-up'" class="fas ml-2"></i>
-            </a>
-        </div>
-        <div :class="open ? 'block': 'hidden'" class="w-full flex-grow sm:flex sm:items-center sm:w-auto">
-            <div class="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 px-6 py-2">
-                <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Technology <i</a>
-                <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Automotive</a>
-                <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Finance</a>
-                <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Politics</a>
-                <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Culture</a>
-                <a href="#" class="hover:bg-gray-400 rounded py-2 px-4 mx-2">Sports</a>
-            </div>
-        </div>
-    </nav> -->
-
-
     <div class="container mx-auto flex flex-wrap py-6">
 
         <!-- Post Section -->
         <section class="w-full md:w-2/3 flex flex-col items-center mx-auto  max-w-3xl">
             <div class="flex flex-wrap w-full justify-cetner">
-                <div class="w-2/5 items-center">
-                    <div class="text-gray-700 text-center">
+                <div class="w-2/5 items-center mx-auto">
+                    <div class="text-gray-700 text-center flex flex-col">
                     @if (!empty($fileName))
                     <img src="{{ asset("images/tweets/$fileName") }}" alt="" class="mx-auto rounded border">
-
+                    <a href={{ asset("images/tweets/$fileName") }} class="px-3 py-2 rounded border mx-auto mt-2 bg-blue-400 text-white hover:text-blue-400 hover:bg-white">دانلود</a>
                     @endif
                         {{-- <img src="{{ asset('images/story.png') }}" alt="" class="mx-auto"> --}}
                     </div>
